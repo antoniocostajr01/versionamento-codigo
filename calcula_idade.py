bd667d513f7d79db8f9da2a97ed56631b629ba36
antoniocostajr01 <antonioclaudiocostajr@gmail.com>
2025-03-27 19:48:19 -0300

    segundo

--- calcula_idade.py
+++ calcula_idade.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
 dateOfBirth = input("Type your date of birth: ")
-born = datetime.striptime(dataOfBirth, "%d/%m/%y")
+born = datetime.strptime(dateOfBirth, "%d/%m/%y")
 today = datetime.today()
-age = today.year - dataOfBirth
+age = today.year - dateOfBirth
 
 print(f"You are {age} years old.")
